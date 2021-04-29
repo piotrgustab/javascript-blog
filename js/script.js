27d79a8bb81fb1929d6884c8247cf4af50e1dfe5
Piotr Gustab <pgustab@aol.com>
2021-04-29 17:56:15 -0400

    add class active

--- js/script.js
+++ js/script.js
@@ -7,14 +7,18 @@ document.getElementById('test-button').addEventListener('click', function(){
   const titleClickHandler = function(event){
     console.log('Link was clicked!');
   
-    /* remove class 'active' from all article links  */
+    /* DONE remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
-  
+    /* IN PROGRESS add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.add('active');
     /* remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.titles a.active');
