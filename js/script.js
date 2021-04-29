83c9aa5503c19bac1004c97c44ec21332f5520a4
Piotr Gustab <pgustab@aol.com>
2021-04-29 17:59:59 -0400

    preventDefault

--- js/script.js
+++ js/script.js
@@ -7,13 +7,14 @@ document.getElementById('test-button').addEventListener('click', function(){
   const titleClickHandler = function(event){
     console.log('Link was clicked!');
   
-    /* DONE remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* IN PROGRESS add class 'active' to the clicked link */
+    /*  add class 'active' to the clicked link */
+    event.preventDefault();
     console.log('clickedElement:', clickedElement);
     const activeLinks = document.querySelectorAll('.titles a.active');
 
